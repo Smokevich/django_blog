d32e8ffa60308023930ef2ca4137009cf03c11d8
”Sergey <swapper12@gmail.com>
2023-06-11 23:36:39 +0500

    upd

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce import models as tinymce_models
 
 # Create your models here.
+class UserProfile(models.Model):
+    class Meta:
+        verbose_name = 'Аватар'
+        verbose_name_plural = 'Аватары'
+        
+    user   = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(verbose_name='Аватар', upload_to='upload/avatar')
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    name = models.CharField(verbose_name='Название тега', max_length=100)
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    name            = models.CharField(verbose_name='Название поста', max_length=150)
+    seo_description = models.CharField(verbose_name='Описание поста', max_length=200, blank=True)
+    seo_keys        = models.CharField(verbose_name="Ключевые слова", max_length=300, blank=True)
+    image           = models.ImageField(verbose_name='Картинка поста', upload_to='upload/images')
+    text            = tinymce_models.HTMLField(verbose_name='Текст поста')
+    author_id       = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name='Автор')
+    tag_id          = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тег')
+    created_at      = models.DateTimeField(verbose_name='Пост создан', auto_now_add=True)
+    is_active       = models.BooleanField(verbose_name='Включить отображение', default=False)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/main.html')
+    return render(request, 'blog/home.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'blog/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            return render(request, 'blog/register.html', {'type': 'danger' ,'message': 'Пароль не совпадает, попробуйте еще раз!'})
+        return render(request, 'blog/register.html', {'type': 'success' ,'message': 'Все отлично!'})
+        # User.objects.create_user()
